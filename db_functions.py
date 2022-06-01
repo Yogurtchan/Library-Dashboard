5481b2eb30da970f716b79c1566b579ed628d897
Eduard Naldoza <naldozaeduardpd@gmail.com>
2022-06-01 20:19:35 +0300

    Integrated database functions

--- db_functions.py
+++ db_functions.py
@@ -2,16 +2,20 @@
 #   pip install pandas
 #   pip install mysql-connector-python
 
+from asyncio.windows_events import NULL
+from cmath import log
 from datetime import datetime
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
-dbname = "libdb"
+dbname = "libdb1"
 test_username = "testuser"
 test_password = "testpass"
 test_studID = "301820946"
+test_college = "CLAC"
+test_course = "BS Psychology"
 
 db = mysql.connector.connect(
     host="localhost",
@@ -27,22 +31,16 @@ def dbInit():
     mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {dbname};")
 
     try:
-        mycursor.execute("CREATE TABLE UserData (name VARCHAR(50), password VARCHAR(50), studentID int);")
+        mycursor.execute("CREATE TABLE UserData (name VARCHAR(50), password VARCHAR(50), studentID int, college VARCHAR(50), course VARCHAR(50));")
     except:
         pass
 
     try:
-        mycursor.execute("CREATE TABLE BorrowHistory (borrowID int, studentID int, bookID int, borrowed datetime NOT NULL, duedate datetime NOT NULL);")
+        mycursor.execute("CREATE TABLE Hist=ory (borrowID int, studentID int, bookISBN int, borrowed datetime NOT NULL, duedate datetime NOT NULL, returndate datetime);")
     except:
         pass
-
-    try:
-        mycursor.execute("CREATE TABLE ReturnHistory (borrowID int, studentID int, bookID int, returned datetime NOT NULL);")
-    except:
-        pass
-
     
-    mycursor.execute(f"INSERT INTO UserData (name, password, studentID) VALUES ('{test_username}', '{test_password}', '{test_studID}')")
+    mycursor.execute(f"INSERT INTO UserData (name, password, studentID) VALUES ('{test_username}', '{test_password}', '{test_studID}', '{test_college}', '{test_course}')")
 
     db.commit()
 
@@ -55,8 +53,8 @@ def dbInit():
 def dbGetUser(username):
     final = []
     mycursor.execute(f"""
-    SELECT name, password, studentID
-    FROM UserData
+    SELECT name, password, studentID, college, course
+    FROM userdata
     WHERE name = '{username}'
     """)
 
@@ -65,31 +63,36 @@ def dbGetUser(username):
 
     return final[0]
 
-
-def dbSetUser(user,password,studentID):
+def dbSetUser(user, password, studentID, college, course):
     mycursor.execute(f"""
-    INSERT INTO UserData (name, password, studentID)
-    VALUES ('{user}', '{password}', '{studentID}')
+    INSERT INTO userdata (name, password, studentID, college, course)
+    VALUES ('{user}', '{password}', '{studentID}', '{college}', '{course}')
     """)
     db.commit()
 
-def dbCreateBorrowOrder(borrowID, studentID, bookID, borrowed=datetime.now(), duedate=datetime.now()+relativedelta(months=+1)):
+def dbCreateBorrowOrder(borrowID, studentID, bookISBN, borrowed=datetime.now(), duedate=datetime.now()+relativedelta(months=+1)):
     mycursor.execute("""
-        INSERT INTO BorrowHistory (borrowID, studentID, bookID, borrowed, duedate)
-        VALUES (%s,%s,%s,%s,%s) """, (borrowID, studentID, bookID, borrowed, duedate))
+        INSERT INTO history (borrowID, studentID, bookISBN, borrowed, duedate, returndate)
+        VALUES (%s,%s,%s,%s,%s,NULL) """, (borrowID, studentID, bookISBN, borrowed, duedate))
     db.commit()
 
-def dbCreateReturnOrder(borrowID, studentID, bookID, returned=datetime.now()):
-    mycursor.execute("""
-        INSERT INTO ReturnHistory (borrowID, studentID, bookID, returned)
-        VALUES (%s,%s,%s,%s) """, (1, 202212347, 101, datetime.now()))
-    db.commit()
-
-def dbReadBorrowOrder(borrowID):
+def dbReadAllBorrowOrder():
     final = []
     mycursor.execute(f"""
-    SELECT borrowID, studentID, bookID, borrowed, duedate
-    FROM BorrowHistory
+    SELECT *
+    FROM history;
+    """)
+
+    for x in mycursor:
+        final.append(x)
+
+    return final
+
+def dbReadBorrowOrder_borrowID(borrowID):
+    final = []
+    mycursor.execute(f"""
+    SELECT borrowID, studentID, bookISBN, borrowed, duedate, returndate
+    FROM history
     WHERE borrowID = '{borrowID}'
     """)
 
@@ -98,15 +101,45 @@ def dbReadBorrowOrder(borrowID):
 
     return final[0]
 
-def dbReadReturnOrder(borrowID):
+def dbReadBorrowOrder_studentID(ID):
     final = []
     mycursor.execute(f"""
-    SELECT borrowID, studentID, bookID, returned
-    FROM ReturnHistory
-    WHERE borrowID = '{borrowID}'
+    SELECT borrowID, studentID, bookISBN, borrowed, duedate, returndate
+    FROM history
+    WHERE studentID = {ID}
     """)
 
     for x in mycursor:
         final.append(x)
 
-    return final[0]
+    return final
+
+def dbCreateReturnOrder(borrowID, date=datetime.now()):
+    mycursor.execute("""
+    UPDATE history
+    SET returndate = %s
+    WHERE borrowID = %s
+    """, (date, borrowID))
+    db.commit()
+
+print(dbReadBorrowOrder_studentID(202230456))
+
+# dbCreateBorrowOrder("Af93Da", 202230456, 9789562911122, borrowed=datetime(2022, 5, 16, 1, 1, 1), duedate=datetime(2022, 6, 16, 1, 1, 1))
+# dbCreateBorrowOrder("d68A1e", 202230457, 9781388266783, borrowed=datetime(2022, 4, 29, 1, 1, 1), duedate=datetime(2022, 5, 29, 1, 1, 1))
+# dbCreateBorrowOrder("fF3001", 202230456, 9781107665644, borrowed=datetime(2022, 4, 17, 1, 1, 1), duedate=datetime(2022, 5, 17, 1, 1, 1))
+# dbCreateBorrowOrder("b88ed1", 202230458, 9780020547204, borrowed=datetime(2022, 3, 9, 1, 1, 1), duedate=datetime(2022, 4, 9, 1, 1, 1))
+# dbCreateBorrowOrder("45c338", 202230458, 9789715507813, borrowed=datetime(2022, 5, 23, 1, 1, 1), duedate=datetime(2022, 6, 23, 1, 1, 1))
+# dbCreateBorrowOrder("4aAb56", 202230457, 9781405087247, borrowed=datetime(2022, 4, 30, 1, 1, 1), duedate=datetime(2022, 5, 30, 1, 1, 1))
+# dbCreateBorrowOrder("56bb90", 202230456, 9780717803026, borrowed=datetime(2022, 3, 12, 1, 1, 1), duedate=datetime(2022, 4, 12, 1, 1, 1))
+# dbCreateBorrowOrder("2E781d", 202230457, 9781784162122, borrowed=datetime(2022, 4, 1, 1, 1, 1), duedate=datetime(2022, 5, 1, 1, 1, 1))
+# dbCreateBorrowOrder("33dAe5", 202230456, 9780679734529, borrowed=datetime(2022, 4, 18, 1, 1, 1), duedate=datetime(2022, 5, 18, 1, 1, 1))
+
+# dbCreateReturnOrder("Af93Da", date=datetime(2022, 5, 23, 1, 1, 1))
+# dbCreateReturnOrder("d68A1e", date=datetime(2022, 5, 25, 1, 1, 1))
+# dbCreateReturnOrder("fF3001", date=datetime(2022, 5, 3, 1, 1, 1))
+# dbCreateReturnOrder("b88ed1", date=datetime(2022, 3, 21, 1, 1, 1))
+# dbCreateReturnOrder("45c338", date=datetime(2022, 5, 23, 1, 1, 1))
+# dbCreateReturnOrder("4aAb56", date=datetime(2022, 5, 11, 1, 1, 1))
+# dbCreateReturnOrder("56bb90", date=datetime(2022, 3, 28, 1, 1, 1))
+# dbCreateReturnOrder("2E781d", date=datetime(2022, 4, 24, 1, 1, 1))
+# dbCreateReturnOrder("33dAe5", date=datetime(2022, 5, 9, 1, 1, 1))
