16c1eec02349bffa838212a67637de475b895404
Eduard Naldoza <naldozaeduardpd@gmail.com>
2022-07-02 23:56:43 +0300

    feat: add books json

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,6 +1,7 @@
 function showBorrowScrn(){
     borrowscrn = document.getElementsByClassName('dash-borrow')[0];
     borrowscrn.className = "dash-borrow";
+    getBook();
 }
 
 function hideBorrowScrn(){
@@ -16,4 +17,17 @@ function showReturnScrn(){
 function hideReturnScrn(){
     borrowscrn = document.getElementsByClassName('dash-return')[0];
     borrowscrn.className = borrowscrn.className + " hidden";
+}
+
+function getBook() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','books.json',true);
+    xhr.onload = () => {
+        console.log(this.status);
+        if(this.status == 200) {
+            console.log('poop');
+        } else {
+            console.log('bad poop');
+        }
+    }
 }
